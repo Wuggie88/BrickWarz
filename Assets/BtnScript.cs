dfb85c23d6cc42882af129e0df3db223ed5c62e3
Wuggie <kawog17@student.sdu.dk>
2022-11-29 09:55:14 +0100

    Initial buttons setup and game manager start

---
+++ Assets/BtnScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnScript : MonoBehaviour
+{
+    public int teams = 0;
+    public GameObject manager;
+
+    private void Start()
+    {
+        //sets the manager
+        manager = GameObject.Find("Manager");
+    }
+
+    public void sendTeams()
+    {
+        //sends the team amount to the manager
+        manager.GetComponent<ManagerScript>().setTeams(teams);
+    }
+}
---
+++ Assets/ManagerScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    public int teams = 0;
+
+    public void setTeams(int data)
+    {
+        Debug.Log("Manager script reached");
+        teams = data;
+    }
+}
