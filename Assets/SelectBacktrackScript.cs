cb62067f3bea44bf32349b895aff0745c9818c0b
Wuggie <wuggie88@yahoo.co.uk>
2022-12-06 11:49:07 +0100

    UPGRADES BITCHES

--- Assets/SelectBacktrackScript.cs
+++ Assets/SelectBacktrackScript.cs
@@ -93,7 +93,44 @@ public class SelectBacktrackScript : MonoBehaviour
                 break;
             case "UpgradeBtn":
                 parent = buildHolder;
-                //set text in GUI to what building you want to upgrade
+                switch (teamTurn) {
+                    case 1:
+                        for (int i = 0; i < gameController.p1Buildings.Length; i++) {
+                            if (gameController.p1Buildings[i] == "") {
+                                spots[i].SetActive(false);
+                            } else {
+                                spots[i].SetActive(true);
+                            }
+                        }
+                        break;
+                    case 2:
+                        for (int i = 0; i < gameController.p2Buildings.Length; i++) {
+                            if (gameController.p2Buildings[i] == "") {
+                                spots[i].SetActive(false);
+                            } else {
+                                spots[i].SetActive(true);
+                            }
+                        }
+                        break;
+                    case 3:
+                        for (int i = 0; i < gameController.p3Buildings.Length; i++) {
+                            if (gameController.p3Buildings[i] == "") {
+                                spots[i].SetActive(false);
+                            } else {
+                                spots[i].SetActive(true);
+                            }
+                        }
+                        break;
+                    case 4:
+                        for (int i = 0; i < gameController.p4Buildings.Length; i++) {
+                            if (gameController.p4Buildings[i] == "") {
+                                spots[i].SetActive(false);
+                            } else {
+                                spots[i].SetActive(true);
+                            }
+                        }
+                        break;
+                }
                 break;
             case "AttackBtn":
                 parent = actionHolder;
@@ -229,7 +266,263 @@ public class SelectBacktrackScript : MonoBehaviour
             gameManager.GetComponent<GameControllerScript>().Turn();
             roundStartHolder.SetActive(true);
             selectSpotHolder.SetActive(false);
-        } else {
+        } else if (selector.name == "UpgradeBtn") {
+            switch (teamTurn) {
+                case 1:
+                    switch (spot.name) {
+                        case "Spot1Btn":
+                            switch (gameController.p1Buildings[0]) {
+                                case "A1":
+                                    gameController.p1Buildings[0] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p1Buildings[0] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p1Buildings[0] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p1Buildings[0] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    
+
+                                    break;
+                            }
+                            break;
+                        case "Spot2Btn":
+                            switch (gameController.p1Buildings[1]) {
+                                case "A1":
+                                    gameController.p1Buildings[1] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p1Buildings[1] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p1Buildings[1] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p1Buildings[1] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot3Btn":
+                            switch (gameController.p1Buildings[2]) {
+                                case "A1":
+                                    gameController.p1Buildings[2] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p1Buildings[2] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p1Buildings[2] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p1Buildings[2] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (spot.name) {
+                        case "Spot1Btn":
+                            switch (gameController.p2Buildings[0]) {
+                                case "A1":
+                                    gameController.p2Buildings[0] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p2Buildings[0] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p2Buildings[0] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p2Buildings[0] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot2Btn":
+                            switch (gameController.p2Buildings[1]) {
+                                case "A1":
+                                    gameController.p2Buildings[1] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p2Buildings[1] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p2Buildings[1] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p2Buildings[1] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot3Btn":
+                            switch (gameController.p2Buildings[2]) {
+                                case "A1":
+                                    gameController.p2Buildings[2] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p2Buildings[2] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p2Buildings[2] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p2Buildings[2] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (spot.name) {
+                        case "Spot1Btn":
+                            switch (gameController.p3Buildings[0]) {
+                                case "A1":
+                                    gameController.p3Buildings[0] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p3Buildings[0] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p3Buildings[0] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p3Buildings[0] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot2Btn":
+                            switch (gameController.p3Buildings[1]) {
+                                case "A1":
+                                    gameController.p3Buildings[1] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p3Buildings[1] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p3Buildings[1] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p3Buildings[1] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot3Btn":
+                            switch (gameController.p3Buildings[2]) {
+                                case "A1":
+                                    gameController.p3Buildings[2] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p3Buildings[2] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p3Buildings[2] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p3Buildings[2] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+
+                case 4:
+                    switch (spot.name) {
+                        case "Spot1Btn":
+                            switch (gameController.p4Buildings[0]) {
+                                case "A1":
+                                    gameController.p4Buildings[0] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p4Buildings[0] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p4Buildings[0] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p4Buildings[0] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot2Btn":
+                            switch (gameController.p4Buildings[1]) {
+                                case "A1":
+                                    gameController.p4Buildings[1] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p4Buildings[1] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p4Buildings[1] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p4Buildings[1] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                        case "Spot3Btn":
+                            switch (gameController.p4Buildings[2]) {
+                                case "A1":
+                                    gameController.p4Buildings[2] = "A2";
+                                    break;
+                                case "A2":
+                                    gameController.p4Buildings[2] = "A3";
+                                    break;
+                                case "D1":
+                                    gameController.p4Buildings[2] = "D2";
+                                    break;
+                                case "D2":
+                                    gameController.p4Buildings[2] = "D3";
+                                    break;
+                                default:
+                                    gameController.teamTurn -= 1;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+            }
+
+            gameManager.GetComponent<GameControllerScript>().Turn();
+            roundStartHolder.SetActive(true);
+            selectSpotHolder.SetActive(false);
+
+        }
+        else {
             //do build, upgrade or actual atack function
             Debug.Log("does action! next teams turn!");
             //Goes to next teams turn
